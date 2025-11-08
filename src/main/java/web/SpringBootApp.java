e02dc6a0423e5bdaad02354a42fc15b958d2a0a2
Slavikonn <slavikon132@gmail.com>
2025-11-08 16:45:38 +0200

    add main Spring Boot application

---
+++ src/main/java/web/SpringBootApp.java
@@ -0,0 +1,13 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+
+}
